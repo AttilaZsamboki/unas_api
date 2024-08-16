9e5304da9336dbc243800ce428246d3bf8239f4b
AttilaZsamboki <zsamboki.attila.jr@gmail.com>
2024-08-16 08:00:53 +0200

    product API

--- src/test/another_file.py
+++ src/test/another_file.py
@@ -1,4 +1,12 @@
-from ..unas_api.unas import CategoriesAPI
+from ..unas_api.unas import UnasAPIBase, Product
 
-category = CategoriesAPI("cfcdf8a7109a30971415ff7f026becdc50dbebbd").get_category("212349")
-print(category)
+unas_client = UnasAPIBase("cfcdf8a7109a30971415ff7f026becdc50dbebbd")
+product = unas_client.get_product("CS-F2811", "full")
+product.categories = [
+    Product.Category("base", "684112", "UTOLSÓ DARABOK"),
+    *product.categories,
+]
+product.remove_category(684112)
+product.categories[0].type = "base"
+product.action = "modify"
+resp = unas_client.set_product(product)
